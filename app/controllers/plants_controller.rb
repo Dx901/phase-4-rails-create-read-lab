8dc666b8d911cc065081dcb81cb33a9d1f64d95c
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-11-22 09:17:21 +0300

    DONE

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,31 @@
+class PlantsController < ApplicationController
+
+    #Get
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    #Get by id
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: "plant not found" }, status: :not_found
+        end
+        
+    end
+    
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+    
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
